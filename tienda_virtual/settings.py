3f167bdec11e47b0a200eaa32522e81c18b23b71
ozmarshall <parc.tec20@yahoo.com>
2022-05-21 00:13:18 -0500

    creando auth y comprobante

--- tienda_virtual/settings.py
+++ tienda_virtual/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-from datetime import timedelta
 from pathlib import Path
 from os import environ
 from dotenv import load_dotenv
